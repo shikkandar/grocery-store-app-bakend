8bcdca642e1d3a440dc69214a7a40c7c462b7cec
shikkandar <mohammedshikkandar1@gmail.com>
2024-08-15 18:11:13 +0530

    initial commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = {
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    },
+  },
+};
